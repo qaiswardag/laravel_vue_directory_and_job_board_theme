3c0c05272214905eecfc5a5b474644ffc68314d6
Qais Wardag <qais.wardag@outlook.com>
2023-04-30 13:32:07 +0200

    Enforce https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,8 +25,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if (env("APP_ENV") === "production") {
-            // URL::forceScheme("https");
-            // $this->app['debugbar']->disable();
+            URL::forceScheme("https");
+            $this->app["debugbar"]->disable();
         }
     }
 }
