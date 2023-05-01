b2624471ecd6035bce499ac054f7664f60522ae5
Qais Wardag <qais.wardag@outlook.com>
2023-05-01 10:56:54 +0200

    Add base URL configuration for Axios in app.js file to resolve mixed content error in Vue.js components

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,7 @@ import { createApp, h } from "vue";
 import { createInertiaApp, Link } from "@inertiajs/vue3";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { ZiggyVue } from "../../vendor/tightenco/ziggy/dist/vue.m";
+import axios from "axios";
 
 const appName =
     window.document.getElementsByTagName("title")[0]?.innerText || "Laravel";
@@ -20,6 +21,8 @@ createInertiaApp({
             import.meta.glob("./Pages/**/*.vue")
         ),
     setup({ el, App, props, plugin }) {
+        axios.defaults.baseURL = import.meta.env.VITE_APP_URL; // set the baseURL for axios
+
         return createApp({ render: () => h(App, props) })
             .use(store)
             .use(plugin)
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -12,9 +12,6 @@ window.axios = axios;
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
-// Set Axios baseURL using VITE_APP_URL environment variable
-window.axios.defaults.baseURL = import.meta.env.VITE_APP_URL;
-
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
