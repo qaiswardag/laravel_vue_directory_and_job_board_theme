3486bbccf202bb27028c71b46ba57678a8dedf9b
Qais Wardag <qais.wardag@outlook.com>
2023-05-01 10:00:22 +0200

    Add baseURL for mixed content issue

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,4 @@
-import _ from 'lodash';
+import _ from "lodash";
 window._ = _;
 
 /**
@@ -7,10 +7,13 @@ window._ = _;
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-import axios from 'axios';
+import axios from "axios";
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+// Set Axios baseURL using VITE_APP_URL environment variable
+window.axios.defaults.baseURL = import.meta.env.VITE_APP_URL;
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
--- vite.config.js
+++ vite.config.js
@@ -1,46 +1,20 @@
-// import { defineConfig } from "vite";
-// import laravel from "laravel-vite-plugin";
-// import vue from "@vitejs/plugin-vue";
-
-// export default defineConfig({
-//     plugins: [
-//         laravel({
-//             input: "resources/js/app.js",
-//             refresh: true,
-//         }),
-//         vue({
-//             template: {
-//                 transformAssetUrls: {
-//                     base: null,
-//                     includeAbsolute: false,
-//                 },
-//             },
-//         }),
-//     ],
-// });
-
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
 
-export default defineConfig(({ mode }) => {
-    const isProduction = mode === "production";
-
-    return {
-        base: isProduction ? process.env.APP_URL || "/" : "/",
-        plugins: [
-            laravel({
-                input: "resources/js/app.js",
-                refresh: true,
-            }),
-            vue({
-                template: {
-                    transformAssetUrls: {
-                        base: null,
-                        includeAbsolute: false,
-                    },
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: "resources/js/app.js",
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
                 },
-            }),
-        ],
-    };
+            },
+        }),
+    ],
 });
