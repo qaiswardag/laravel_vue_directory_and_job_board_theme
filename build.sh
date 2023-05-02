8ccd45f1dd762909345feaa0b8850939e0501d9a
Qais Wardag <qais.wardag@outlook.com>
2023-05-02 19:46:55 +0200

    Update build.sh script and comments

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+# This is a custom build script for a Laravel project on Digital Ocean App Platform.
+# It installs the required PHP exif extension.
+
 # Update package list
 apt-get update
 
 # Install PHP exif extension
 apt-get install -y php8.2-exif
-
-# Install composer dependencies
-composer install
