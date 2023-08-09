589b02f9428ea65ae9d6a7778a93aa7869aeec63
Qais Wardag <qais.wardag@outlook.com>
2023-08-09 18:58:43 +0200

    Bug fixes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,11 +9,10 @@
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100;0,200;0,300;0,400;0,500;1,100;1,200;1,300;1,400;1,500&display=swap"
-          rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100;0,200;0,300;0,400;0,500;1,100;1,200;1,300;1,400;1,500&display=swap"
+        rel="stylesheet"
         />
-
-
+        <!-- Fonts -->
         <!-- Favicon start -->
         <link
         rel="icon"
