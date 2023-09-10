b85fbf85c76389fbc407f9b2f9b78323be069307
Qais Wardag <qais.wardag@outlook.com>
2023-09-10 11:00:58 +0200

    Move show authors to bottom

--- resources/js/Pages/Jobs/Partials/JobForm.vue
+++ resources/js/Pages/Jobs/Partials/JobForm.vue
@@ -711,6 +711,108 @@ onBeforeMount(() => {
                 <InputError :message="postForm.errors.published" />
             </div>
             <!-- post status - end -->
+
+            <!-- cover image - start -->
+            <div class="myInputsOrganization">
+                <div class="myPrimaryFormOrganizationHeaderDescriptionSection">
+                    <div class="myPrimaryFormOrganizationHeader">
+                        Cover image
+                    </div>
+                    <p class="myPrimaryParagraph">
+                        Uplaod or select a post cover image.
+                    </p>
+                </div>
+
+                <img
+                    v-if="
+                        postForm.cover_image_medium &&
+                        postForm.cover_image_medium.length !== 0
+                    "
+                    @click="handleUploadCoverImage"
+                    class="myPrimarythumbnailInsertPreview"
+                    alt="cover image"
+                    :src="`/storage/uploads/${postForm.cover_image_medium}`"
+                />
+
+                <div
+                    class="myInputGroup flex items-center justify-between border-t border-myPrimaryLightGrayColor pt-4"
+                >
+                    <button
+                        @click="handleUploadCoverImage"
+                        type="button"
+                        class="myPrimaryButton gap-2 items-center"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="w-4 h-4"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+                            />
+                        </svg>
+
+                        Cover image
+                    </button>
+
+                    <div v-if="postForm && postForm.cover_image_medium">
+                        <svg
+                            @click="handleRemoveCoverImage"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="w-5 h-5 text-myPrimaryErrorColor cursor-pointer"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                            />
+                        </svg>
+                    </div>
+                </div>
+                <InputError :message="postForm.errors.cover_image_original" />
+            </div>
+            <!-- cover image - end -->
+            <!-- categories - start -->
+            <div class="myInputsOrganization">
+                <div class="myPrimaryFormOrganizationHeaderDescriptionSection">
+                    <div class="myPrimaryFormOrganizationHeader">
+                        Categories
+                    </div>
+                    <p class="myPrimaryParagraph">
+                        Enter categories for the post.
+                    </p>
+                </div>
+                <div class="myInputGroup">
+                    <InputError :message="postForm.errors.categories" />
+                </div>
+            </div>
+            <!-- categories - end -->
+
+            <!-- tags - start -->
+            <div class="myInputsOrganization">
+                <div class="myPrimaryFormOrganizationHeaderDescriptionSection">
+                    <div class="myPrimaryFormOrganizationHeader">Tags</div>
+                    <p class="myPrimaryParagraph">Enter tags for the post.</p>
+                </div>
+                <div class="myInputGroup">
+                    <Tags
+                        :clearTags="clearTags"
+                        :tagsOnLoad="postForm.tags"
+                        @handleTags="handleTags"
+                    ></Tags>
+                    <InputError :message="postForm.errors.tags" />
+                </div>
+            </div>
+            <!-- tags - end -->
             <!-- post show author - start -->
             <div class="myInputsOrganization">
                 <div class="myPrimaryFormOrganizationHeaderDescriptionSection">
@@ -925,123 +1027,8 @@ onBeforeMount(() => {
                 </div>
             </div>
             <!-- post show author - end -->
-            <!-- cover image - start -->
-            <div class="myInputsOrganization">
-                <div class="myPrimaryFormOrganizationHeaderDescriptionSection">
-                    <div class="myPrimaryFormOrganizationHeader">
-                        Cover image
-                    </div>
-                    <p class="myPrimaryParagraph">
-                        Uplaod or select a post cover image.
-                    </p>
-                </div>
 
-                <img
-                    v-if="
-                        postForm.cover_image_medium &&
-                        postForm.cover_image_medium.length !== 0
-                    "
-                    @click="handleUploadCoverImage"
-                    class="myPrimarythumbnailInsertPreview"
-                    alt="cover image"
-                    :src="`/storage/uploads/${postForm.cover_image_medium}`"
-                />
-
-                <div
-                    class="myInputGroup flex items-center justify-between border-t border-myPrimaryLightGrayColor pt-4"
-                >
-                    <button
-                        @click="handleUploadCoverImage"
-                        type="button"
-                        class="myPrimaryButton gap-2 items-center"
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke-width="1.5"
-                            stroke="currentColor"
-                            class="w-4 h-4"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-                            />
-                        </svg>
-
-                        Cover image
-                    </button>
-
-                    <div v-if="postForm && postForm.cover_image_medium">
-                        <svg
-                            @click="handleRemoveCoverImage"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke-width="1.5"
-                            stroke="currentColor"
-                            class="w-5 h-5 text-myPrimaryErrorColor cursor-pointer"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                            />
-                        </svg>
-                    </div>
-                </div>
-                <InputError :message="postForm.errors.cover_image_original" />
-            </div>
-            <!-- cover image - end -->
-            <!-- categories - start -->
-            <div class="myInputsOrganization">
-                <div class="myPrimaryFormOrganizationHeaderDescriptionSection">
-                    <div class="myPrimaryFormOrganizationHeader">
-                        Categories
-                    </div>
-                    <p class="myPrimaryParagraph">
-                        Enter categories for the post.
-                    </p>
-                </div>
-                <div class="myInputGroup">
-                    <InputError :message="postForm.errors.categories" />
-                </div>
-            </div>
-            <!-- categories - end -->
-
-            <!-- tags - start -->
-            <div class="myInputsOrganization">
-                <div class="myPrimaryFormOrganizationHeaderDescriptionSection">
-                    <div class="myPrimaryFormOrganizationHeader">Tags</div>
-                    <p class="myPrimaryParagraph">Enter tags for the post.</p>
-                </div>
-                <div class="myInputGroup">
-                    <Tags
-                        :clearTags="clearTags"
-                        :tagsOnLoad="postForm.tags"
-                        @handleTags="handleTags"
-                    ></Tags>
-                    <InputError :message="postForm.errors.tags" />
-                </div>
-            </div>
-            <!-- tags - end -->
-            <!-- categories - start -->
-            <div class="myInputsOrganization">
-                <div class="myPrimaryFormOrganizationHeaderDescriptionSection">
-                    <div class="myPrimaryFormOrganizationHeader">
-                        Categories
-                    </div>
-                    <p class="myPrimaryParagraph">
-                        Enter categories for the job.
-                    </p>
-                </div>
-                <div class="myInputGroup">
-                    <InputError :message="postForm.errors.categroies" />
-                </div>
-            </div>
-            <!-- categories - end -->
-
+            <!-- post belongs to team - start -->
             <div class="myInputsOrganization">
                 <div class="myPrimaryFormOrganizationHeaderDescriptionSection">
                     <div class="myPrimaryFormOrganizationHeader">
@@ -1134,6 +1121,7 @@ onBeforeMount(() => {
                     <InputError :message="postForm.errors.team" />
                 </div>
             </div>
+            <!-- post belongs to team - enc -->
         </template>
 
         <template #actions>
