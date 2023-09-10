1ea6b37a1574dfd673f089894e848e5b58ce2f56
Qais Wardag <qais.wardag@outlook.com>
2023-09-10 15:47:56 +0200

    Add job title

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration {
             $table->string("first_name");
             $table->string("last_name");
             $table->string("email")->unique();
+            $table->string("job_title")->nullable();
             $table
                 ->string("username")
                 ->unique()
