2b49f83216f2e9650a5afcc59c3f32e198c5a6a1
Qais Wardag <qais.wardag@outlook.com>
2023-10-05 20:46:24 +0200

    Install Sentry

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -70,6 +70,10 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        //
+        $this->reportable(function (Throwable $e) {
+            if (app()->bound("sentry")) {
+                app("sentry")->captureException($e);
+            }
+        });
     }
 }
