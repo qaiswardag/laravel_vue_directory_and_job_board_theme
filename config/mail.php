5a40adb39fe616dabe3332582fda8f76cb71ee8e
Qais Wardag <qais.wardag@outlook.com>
2023-10-05 23:32:48 +0200

    Work on setting up Mailgun

--- config/mail.php
+++ config/mail.php
@@ -49,9 +49,9 @@ return [
         ],
 
         "mailgun" => [
-            "transport" => "mailgun",
             "domain" => env("MAILGUN_DOMAIN"),
             "secret" => env("MAILGUN_SECRET"),
+            "endpoint" => env("MAILGUN_ENDPOINT", "api.eu.mailgun.net"),
         ],
 
         "postmark" => [
