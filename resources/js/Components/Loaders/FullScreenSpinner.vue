2d0beae6114e27e4bd22ed3bc1fc9e287504f79e
Qais Wardag <qais.wardag@outlook.com>
2023-10-07 12:57:29 +0200

    Update css styles for full screen spinner

--- resources/js/Components/Loaders/FullScreenSpinner.vue
+++ resources/js/Components/Loaders/FullScreenSpinner.vue
@@ -4,7 +4,7 @@
             class="flex justify-center items-center h-screen w-screen fixed inset-0 z-50 bg-white"
         >
             <div
-                class="inline-block h-12 w-12 animate-spin rounded-full border-4 border-solid border-current border-r-transparent"
+                class="inline-block h-10 w-10 animate-spin rounded-full border-4 border-solid border-current border-r-transparent"
                 role="status"
             ></div>
         </div>
