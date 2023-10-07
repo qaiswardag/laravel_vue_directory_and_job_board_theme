a7163caf6f107824c03dae8c1b6b6bf787aef8ae
Qais Wardag <qais.wardag@outlook.com>
2023-10-07 12:56:55 +0200

    Update css styles for full screen spinner

--- resources/js/Components/Loaders/FullScreenSpinner.vue
+++ resources/js/Components/Loaders/FullScreenSpinner.vue
@@ -4,14 +4,9 @@
             class="flex justify-center items-center h-screen w-screen fixed inset-0 z-50 bg-white"
         >
             <div
-                class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                class="inline-block h-12 w-12 animate-spin rounded-full border-4 border-solid border-current border-r-transparent"
                 role="status"
-            >
-                <span
-                    class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
-                    >Loading...</span
-                >
-            </div>
+            ></div>
         </div>
     </teleport>
 </template>
