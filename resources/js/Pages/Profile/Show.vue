734e2ecabc74a342240843915047fe904be5173e
Qais Wardag <qais.wardag@outlook.com>
2023-10-20 10:20:56 +0200

    Move payments methods to own section

--- resources/js/Pages/Profile/Show.vue
+++ resources/js/Pages/Profile/Show.vue
@@ -289,10 +289,61 @@ const updatePaymentMethod = function () {
                         </span>
                     </div>
                 </div>
-                <SectionBorder description="Billing"></SectionBorder>
+                <SectionBorder
+                    description="Billing & subscriptions"
+                ></SectionBorder>
                 <div
                     class="divide-y divide-gray-200 sm:grid sm:grid-cols-2 sm:gap-px sm:divide-y-0"
                 >
+                    <!-- Payments and invoices # start -->
+                    <div
+                        class="relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-myPrimaryBrandColor cursor-pointer"
+                    >
+                        <div>
+                            <span
+                                class="h-10 w-10 cursor-pointer rounded-full flex items-center justify-center bg-gray-50 aspect-square hover:bg-myPrimaryLinkColor hover:text-white focus-visible:ring-0"
+                            >
+                                <BanknotesIcon
+                                    class="myMediumIcon"
+                                ></BanknotesIcon>
+                            </span>
+                        </div>
+                        <div class="mt-8">
+                            <h3 class="text-lg font-normal">
+                                <Link
+                                    :href="route('stripe.payments.index')"
+                                    class="focus:outline-none"
+                                >
+                                    <span
+                                        class="absolute inset-0"
+                                        aria-hidden="true"
+                                    />
+                                    Payments and invoices
+                                </Link>
+                            </h3>
+                            <p class="mt-2 text-sm text-gray-500">
+                                Doloribus dolores nostrum quia qui natus officia
+                                quod et dolorem. Sit repellendus qui ut at
+                                blanditiis et quo et molestiae.
+                            </p>
+                        </div>
+                        <span
+                            class="pointer-events-none absolute top-6 right-6 text-gray-300 group-hover:text-gray-400"
+                            aria-hidden="true"
+                        >
+                            <svg
+                                class="h-6 w-6"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor"
+                                viewBox="0 0 24 24"
+                            >
+                                <path
+                                    d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z"
+                                />
+                            </svg>
+                        </span>
+                    </div>
+                    <!-- Payments and invoices # end -->
                     <!-- subscriptions # start -->
                     <div
                         class="relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-myPrimaryBrandColor cursor-pointer"
@@ -346,55 +397,11 @@ const updatePaymentMethod = function () {
                         </span>
                     </div>
                     <!-- subscriptions # end -->
-                    <!-- Payments # start -->
-                    <div
-                        class="relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-myPrimaryBrandColor cursor-pointer"
-                    >
-                        <div>
-                            <span
-                                class="h-10 w-10 cursor-pointer rounded-full flex items-center justify-center bg-gray-50 aspect-square hover:bg-myPrimaryLinkColor hover:text-white focus-visible:ring-0"
-                            >
-                                <BanknotesIcon
-                                    class="myMediumIcon"
-                                ></BanknotesIcon>
-                            </span>
-                        </div>
-                        <div class="mt-8">
-                            <h3 class="text-lg font-normal">
-                                <Link
-                                    :href="route('stripe.payments.index')"
-                                    class="focus:outline-none"
-                                >
-                                    <span
-                                        class="absolute inset-0"
-                                        aria-hidden="true"
-                                    />
-                                    Payments and invoices
-                                </Link>
-                            </h3>
-                            <p class="mt-2 text-sm text-gray-500">
-                                Doloribus dolores nostrum quia qui natus officia
-                                quod et dolorem. Sit repellendus qui ut at
-                                blanditiis et quo et molestiae.
-                            </p>
-                        </div>
-                        <span
-                            class="pointer-events-none absolute top-6 right-6 text-gray-300 group-hover:text-gray-400"
-                            aria-hidden="true"
-                        >
-                            <svg
-                                class="h-6 w-6"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="currentColor"
-                                viewBox="0 0 24 24"
-                            >
-                                <path
-                                    d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z"
-                                />
-                            </svg>
-                        </span>
-                    </div>
-                    <!-- Payments # end -->
+                </div>
+                <SectionBorder description="Payment methods"></SectionBorder>
+                <div
+                    class="divide-y divide-gray-200 sm:grid sm:grid-cols-2 sm:gap-px sm:divide-y-0"
+                >
                     <!-- Update payment methods # start -->
                     <div
                         v-if="intent"
