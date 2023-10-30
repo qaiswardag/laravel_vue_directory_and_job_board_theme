2dd46621beb48be1118794939cdf5239780a1e45
Qais Wardag <qais.wardag@outlook.com>
2023-10-30 01:07:55 +0100

    Update default content html code template for post, job and store

--- database/factories/Job/JobFactory.php
+++ database/factories/Job/JobFactory.php
@@ -69,7 +69,8 @@ class JobFactory extends Factory
             "deleted_at" => null,
             "title" => $title,
             "slug" => $slug,
-            "content" => $this->faker->sentence(rand(20, 60), false),
+            "content" =>
+                "<section <div class=\"w-full md:pt-12 md:pb-12 pt-4 pb-4\"><div class=\"mx-auto max-w-7xl\" element=\"\"><div class=\"myPrimaryGap grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-2\" element=\"\"> <div class=\"flex-1 py-2\" element=\"\"> <img class=\"object-cover w-full object-top aspect-square smooth-transition\" src=\"/storage/uploads/fake-images/stores/13.jpg\" alt=\"provider\" element=\"\" hovered=\"\"> <div class=\"break-all py-2\" element=\"\"><p element=\"\">Layouts and visual.</p><p element=\"\">Ullamcorper dignissim cras tincidunt.</p></div> </div> <div class=\"flex-1 py-2\" element=\"\"> <img class=\"object-cover w-full object-top aspect-square smooth-transition\" src=\"/storage/uploads/fake-images/stores/14.jpg\" alt=\"provider\" element=\"\"> <div class=\"break-all py-2\" element=\"\"><p element=\"\">Layouts and visual.</p><p element=\"\">Ullamcorper dignissim cras tincidunt.</p></div> </div>   </div> </div> </div></section>",
             "published" => rand(0, 1),
             "tags" => $selectedTagsList,
         ];
--- database/factories/Store/StoreFactory.php
+++ database/factories/Store/StoreFactory.php
@@ -62,9 +62,7 @@ class StoreFactory extends Factory
         );
 
         $content =
-            "<section><div class=\"w-full pt-32 pb-32\"><div class=\"mx-auto max-w-7xl px-6 lg:flex lg:px-8\">" .
-            $this->faker->sentence(rand(20, 60), false) .
-            "</div></div></section>";
+            "<section <div class=\"w-full md:pt-12 md:pb-12 pt-4 pb-4\"><div class=\"mx-auto max-w-7xl\" element=\"\"><div class=\"myPrimaryGap grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-2\" element=\"\"> <div class=\"flex-1 py-2\" element=\"\"> <img class=\"object-cover w-full object-top aspect-square smooth-transition\" src=\"/storage/uploads/fake-images/stores/13.jpg\" alt=\"provider\" element=\"\" hovered=\"\"> <div class=\"break-all py-2\" element=\"\"><p element=\"\">Layouts and visual.</p><p element=\"\">Ullamcorper dignissim cras tincidunt.</p></div> </div> <div class=\"flex-1 py-2\" element=\"\"> <img class=\"object-cover w-full object-top aspect-square smooth-transition\" src=\"/storage/uploads/fake-images/stores/14.jpg\" alt=\"provider\" element=\"\"> <div class=\"break-all py-2\" element=\"\"><p element=\"\">Layouts and visual.</p><p element=\"\">Ullamcorper dignissim cras tincidunt.</p></div> </div>   </div> </div> </div></section>";
 
         return [
             "team_id" => 2,
