36692824cb57e6d1c22c7a06868ab3dd87dba716
Qais Wardag <qais.wardag@outlook.com>
2023-11-17 09:25:34 +0100

    Use checkbox for is filled

--- resources/js/Pages/Jobs/Partials/JobForm.vue
+++ resources/js/Pages/Jobs/Partials/JobForm.vue
@@ -1426,81 +1426,26 @@ const pageBuilder = new PageBuilder(store);
                         }}
                     </p>
                 </div>
-                <div
-                    class="myInputGroup flex myPrimaryGap flex-row-reverse justify-end"
-                >
-                    <InputLabel
-                        :value="postForm.is_filled ? 'Filled' : 'Not filled'"
-                        :class="{
-                            'text-myPrimaryLinkColor': !postForm.is_filled,
-                            'text-myPrimaryErrorColor': postForm.is_filled,
-                        }"
-                    />
-
-                    <Switch
-                        v-model="postForm.is_filled"
-                        :class="[
-                            postForm.is_filled
-                                ? 'bg-myPrimaryErrorColor'
-                                : 'bg-gray-200',
-                            'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-myPrimaryErrorColor focus:ring-offset-2',
-                        ]"
-                    >
-                        <span class="sr-only">Use setting</span>
-                        <span
-                            :class="[
-                                postForm.is_filled
-                                    ? 'translate-x-5'
-                                    : 'translate-x-0',
-                                'pointer-events-none relative inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out',
-                            ]"
-                        >
-                            <span
-                                :class="[
-                                    postForm.is_filled
-                                        ? 'opacity-0 ease-out duration-100'
-                                        : 'opacity-100 ease-in duration-200',
-                                    'absolute inset-0 flex h-full w-full items-center justify-center transition-opacity',
-                                ]"
-                                aria-hidden="true"
+                <div class="myInputGroup">
+                    <div class="relative flex items-start py-4">
+                        <div class="min-w-0 flex-1 text-sm leading-6">
+                            <label
+                                for="is_filled"
+                                class="select-none font-medium text-gray-900"
+                                >Is filled</label
                             >
-                                <svg
-                                    class="h-3 w-3 text-gray-400"
-                                    fill="none"
-                                    viewBox="0 0 12 12"
-                                >
-                                    <path
-                                        d="M4 8l2-2m0 0l2-2M6 6L4 4m2 2l2 2"
-                                        stroke="currentColor"
-                                        stroke-width="1.5"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                    />
-                                </svg>
-                            </span>
-                            <span
-                                :class="[
-                                    postForm.is_filled
-                                        ? 'opacity-100 ease-in duration-200'
-                                        : 'opacity-0 ease-out duration-100',
-                                    'absolute inset-0 flex h-full w-full items-center justify-center transition-opacity',
-                                ]"
-                                aria-hidden="true"
-                            >
-                                <svg
-                                    class="h-3 w-3 text-myPrimaryErrorColor"
-                                    fill="currentColor"
-                                    viewBox="0 0 12 12"
-                                >
-                                    <path
-                                        d="M3.707 5.293a1 1 0 00-1.414 1.414l1.414-1.414zM5 8l-.707.707a1 1 0 001.414 0L5 8zm4.707-3.293a1 1 0 00-1.414-1.414l1.414 1.414zm-7.414 2l2 2 1.414-1.414-2-2-1.414 1.414zm3.414 2l4-4-1.414-1.414-4 4 1.414 1.414z"
-                                    />
-                                </svg>
-                            </span>
-                        </span>
-                    </Switch>
+                        </div>
+                        <div class="ml-3 flex h-6 items-center">
+                            <input
+                                id="is_filled"
+                                name="is_filled"
+                                v-model="postForm.is_filled"
+                                type="checkbox"
+                                class="h-6 w-6 rounded border-gray-300 text-myPrimaryBrandColor focus:ring-myPrimaryBrandColor"
+                            />
+                        </div>
+                    </div>
                 </div>
-                <InputError :message="postForm.errors.is_filled" />
             </div>
             <!-- post is filled - end -->
 
