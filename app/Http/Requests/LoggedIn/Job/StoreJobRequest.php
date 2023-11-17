203eb477637e87855c42f77cf49f472a2917ae52
Qais Wardag <qais.wardag@outlook.com>
2023-11-18 00:43:25 +0100

    Update started at date validation

--- app/Http/Requests/LoggedIn/Job/StoreJobRequest.php
+++ app/Http/Requests/LoggedIn/Job/StoreJobRequest.php
@@ -149,10 +149,7 @@ class StoreJobRequest extends FormRequest
                 $this->started_at &&
                 !$this->is_paid &&
                 Carbon::parse($this->started_at)->isValid() &&
-                Carbon::parse($this->started_at)->isPast() &&
-                Carbon::parse($this->started_at)->diffInDays(
-                    Carbon::now()->subDay()
-                ) > 0
+                !Carbon::parse($this->started_at)->addDays(1)->isFuture()
             ) {
                 $validator
                     ->errors()
@@ -161,7 +158,7 @@ class StoreJobRequest extends FormRequest
                         "The started at date must be from today and in the future."
                     );
             }
-            // The Started at date must be in the future
+            // The started at date must be in the past and can be up to 30 days old from today.
             if (
                 $this->job && $this->job->id &&
                 $this->started_at &&
