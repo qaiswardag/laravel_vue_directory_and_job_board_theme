7fb0e71d44a17073d07190f84e7946cbee080117
Qais Wardag <qais.wardag@outlook.com>
2024-01-09 20:25:56 +0100

    Feature Cookie Modal

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -2,9 +2,10 @@
 import { router } from "@inertiajs/vue3";
 import FullScreenSpinner from "@/Components/Loaders/FullScreenSpinner.vue";
 import Flash from "@/Components/Actions/Flash.vue";
-import { ref, computed, watch } from "vue";
+import { ref, computed, watch, onMounted } from "vue";
 import { usePage } from "@inertiajs/vue3";
 import { useStore } from "vuex";
+import CookieConsentModal from "@/Components/Modals/CookieConsentModal.vue";
 
 defineProps({
     title: {
@@ -41,6 +42,38 @@ watch(flashState, (newValue) => {
         shouldShowFlash.value = false;
     }, 1000);
 });
+
+//
+//
+//
+const showCookieModal = ref(false);
+const cookieName = "cookieConsent";
+
+const handleCookieModal = function () {
+    showCookieModal.value = false;
+};
+
+const checkACookieExists = function (cookieName) {
+    if (
+        document.cookie
+            .split(";")
+            .some((item) => item.trim().startsWith(`${cookieName}=`))
+    ) {
+        showCookieModal.value = false;
+    } else {
+        showCookieModal.value = true;
+    }
+};
+
+//
+//
+//
+//
+onMounted(() => {
+    if (checkACookieExists(cookieName)) {
+        // showCookieModal.value = true;
+    }
+});
 </script>
 
 <template>
@@ -53,6 +86,9 @@ watch(flashState, (newValue) => {
     <teleport to="body">
         <FullScreenSpinner v-if="getIsLoading"></FullScreenSpinner>
     </teleport>
-
     <slot />
+    <CookieConsentModal
+        :show="showCookieModal"
+        @handleCookieModal="handleCookieModal"
+    ></CookieConsentModal>
 </template>
