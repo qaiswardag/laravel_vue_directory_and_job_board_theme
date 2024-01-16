60d5ce9a8de66a532359c81e139b00c0131e37f0
Qais Wardag <qais.wardag@outlook.com>
2024-01-16 10:38:17 +0100

    Enhance user tag design

--- resources/js/Components/Users/UserTag.vue
+++ resources/js/Components/Users/UserTag.vue
@@ -30,8 +30,7 @@ const props = defineProps({
         v-if="user"
         :class="[
             {
-                'hover:bg-myPrimaryLinkColor hover:text-white cursor-pointer':
-                    clickable,
+                'hover:bg-opacity-60 cursor-pointer': clickable,
             },
 
             customClass,
@@ -67,7 +66,7 @@ const props = defineProps({
                         Array.isArray(user.user_photo) &&
                         user.user_photo.length === 0
                     "
-                    class="px-2"
+                    class="pl-2"
                 >
                     <div
                         class="text-white flex-shrink-0 h-8 w-8 rounded-full bg-myPrimaryBrandColor flex justify-center items-center text-xs rounded-l-full"
@@ -84,23 +83,23 @@ const props = defineProps({
                 class=""
             >
                 <div
-                    class="flex flex-col items-start justify-center bg-stone-300 min-h-[3rem] px-2"
+                    class="flex flex-col items-start justify-center min-h-[3rem] px-2 text-myPrimaryDarkGrayColor"
                     :class="[
                         user.user_photo ? 'rounded-r-full' : 'rounded-full',
                     ]"
                 >
-                    <span class="myPrimaryParagraph font-medium h-full text-sm">
+                    <span class="font-medium h-full text-sm">
                         {{ user.first_name }}
                         {{ user.last_name }}
                     </span>
                     <template v-if="showJobTitle && user.job_title">
-                        <span class="myPrimaryParagraph text-xs font-normal">
+                        <span class="text-xs font-normal">
                             {{ `title: ${user.job_title}` }}
                         </span>
                     </template>
 
                     <template v-if="showTeamRole && currentUserTeamRole">
-                        <span class="myPrimaryParagraph text-xs font-normal">
+                        <span class="text-xs font-normal">
                             role:
                             {{
                                 currentUserTeamRole?.key
