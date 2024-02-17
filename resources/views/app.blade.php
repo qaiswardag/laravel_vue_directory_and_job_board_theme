46ad296d4bda063645afa5ca36ea823839cc3466
Qais Wardag <qais.wardag@outlook.com>
2024-02-17 19:13:28 +0100

    Remove G tag

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -27,15 +27,6 @@
         async src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAzutbgFidPQIG0Gkaqx8DmiwiB0pUFyeE&libraries=places&callback=console.debug"></script>
         <!-- Google maps end -->
 
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-DJ32Y8NV9M"></script>
-        <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-DJ32Y8NV9M');
-        </script>
-
         <!-- Favicon start -->
         <link
         rel="icon"
