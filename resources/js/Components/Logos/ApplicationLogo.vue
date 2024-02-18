789e3efd9b3dfe59614c05b931bfbeca841b2c70
Qais Wardag <qais.wardag@outlook.com>
2024-02-18 16:04:59 +0100

    Logo click route to main site

--- resources/js/Components/Logos/ApplicationLogo.vue
+++ resources/js/Components/Logos/ApplicationLogo.vue
@@ -1,11 +1,29 @@
-<script setup></script>
+<script setup>
+import { usePage } from "@inertiajs/vue3";
+
+const goToHome = function () {
+    if (
+        usePage() &&
+        usePage().props &&
+        usePage().props.environment === "local"
+    ) {
+        window.location.href = "http://localhost:3000";
+    }
+    if (
+        usePage() &&
+        usePage().props &&
+        usePage().props.environment !== "local"
+    ) {
+        window.location.href = "https://www.myself.ae";
+    }
+};
+</script>
 
 <template>
-    <Link :href="route('home')" class="myPrimaryParagraph">
-        <img
-            class="lg:w-28 w-24"
-            src="/logo/myself_text_logo_black.svg"
-            alt="Logo"
-        />
-    </Link>
+    <img
+        @click="goToHome"
+        class="lg:w-28 w-24 cursor-pointer"
+        src="/logo/myself_text_logo_black.svg"
+        alt="Logo"
+    />
 </template>
