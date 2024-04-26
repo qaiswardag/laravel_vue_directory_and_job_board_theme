6f7f6e465fdd48a831641512cb7bbca198a99fd1
Qais Wardag <qais.wardag@outlook.com>
2024-04-26 22:04:01 +0200

    Add footer

--- resources/js/Components/Footer/FooterGuests.vue
+++ resources/js/Components/Footer/FooterGuests.vue
@@ -15,6 +15,14 @@
                     GitHub
                 </a>
             </p>
+            <p class="myPrimaryParagraph lg:text-sm text-xs">
+                <a
+                    href="https://www.linkedin.com/in/qaiswardag/"
+                    target="_blank"
+                >
+                    LinkedIn
+                </a>
+            </p>
         </div>
     </footer>
 </template>
