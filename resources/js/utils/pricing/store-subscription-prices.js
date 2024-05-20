72c0fae5b9551ce285abfe3cc2bf2be02ae50651
Qais Wardag <qais.wardag@outlook.com>
2024-05-20 18:58:10 +0200

    Update product price id

--- resources/js/utils/pricing/store-subscription-prices.js
+++ resources/js/utils/pricing/store-subscription-prices.js
@@ -67,8 +67,8 @@ const listingsubscriptionPrices = [
         dynamic_product: true,
         minimum_quantity: 4,
         maximum_quantity: 1000,
-        priceIdentifierStripe: "price_1PIUp01aEOmwlrqICq8iRT3C",
-        priceIdentifierStripeTest: "1234",
+        priceIdentifierStripe: "price_1PIUpm1aEOmwlrqIAtpM3ipd",
+        priceIdentifierStripeTest: "price_1PIUpm1aEOmwlrqIAtpM3ipd",
         route: {
             name: "stripe.listings.create.subscription",
             parameters: [],
