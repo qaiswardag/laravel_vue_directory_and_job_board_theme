f7556fa02f87e64f8656ef886872e7977760784b
Qais Wardag <qais.wardag@outlook.com>
2025-03-16 13:23:48 +0100

    Remove unused import of package.json in Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
-import packageJson from "./package.json";
 
 export default defineConfig({
     plugins: [
